4cba374561670c67c120201c1b025186bc62e1d2
Hugo Loiola <hugoloiola.1@gmail.com>
2025-06-05 18:55:33 -0300

    banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,11 @@ import { UsersModule } from './users/users.module';
   imports: [
     // Configuração básica do TypeORM
     TypeOrmModule.forRoot({
-      type: 'postgres', // ou mysql, maria, etc.
+      type: 'mysql', // ou mysql, maria, etc.
       host: 'localhost',
-      port: 5432, // porta do Postgres
-      username: 'seu_usuario', // ajuste para seu DB
-      password: 'sua_senha',
+      port: 3306, // porta do MySQL
+      username: 'ifruta', // ajuste para seu DB
+      password: 'laele',
       database: 'ifruita_db', // crie este banco antes
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // No dev, true; em produção, false.
